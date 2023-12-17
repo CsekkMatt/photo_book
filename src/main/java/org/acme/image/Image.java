fe840bb06d0ff5cb79ea9272e0f7d4685be52b60
mpetok <matepetok@gmail.com>
2023-12-17 14:07:37 +0200

    adjusted Dockerfile for build

--- src/main/java/org/acme/image/Image.java
+++ src/main/java/org/acme/image/Image.java
@@ -4,10 +4,12 @@ import java.time.LocalDateTime;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "Image")
 @Setter
 @Getter
 public class Image extends PanacheEntity {
@@ -16,7 +18,7 @@ public class Image extends PanacheEntity {
     private LocalDateTime uploadDateTime;
 
     public Image() {
-
+        ///
     }
 
     public Image(String fileName, String fileType, LocalDateTime uploadDateTime) {
